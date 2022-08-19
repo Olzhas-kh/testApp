2c41c93f4552fe35dd067ab215e3e77fade26431
Aboneyx <abylay.asylbek02@gmail.com>
2022-08-19 13:44:15 +0600

    no msg

--- lib/src/core/network/network_helper.dart
+++ lib/src/core/network/network_helper.dart
@@ -1,6 +1,9 @@
 mixin EndPoints {
   static const baseUrl = 'https://dev.api.narxozapp.kz/v1/mobile';
 
+  // help section
+  static const helpSections = '/dormitories/help-sections';
+
   // orders folder
   static const dormitoriesInfo = '/dormitories/information';
   static const dormitoriesOrdersDegree = '/dormitories/orders/degrees';
