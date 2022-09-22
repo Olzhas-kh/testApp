8a6017518a3ae1538bd106fe9c84d72428d16a1f
Aboneyx <abylay.asylbek02@gmail.com>
2022-09-22 07:22:44 +0600

    no message

--- lib/src/feautures/notifications/notifications.dart
+++ lib/src/feautures/notifications/notifications.dart
@@ -1,6 +1,7 @@
 /// Models
 export 'api/models/notification.dart';
 
+// ignore: directives_ordering
 /// Widgets
 export './widget/pages/notifications.dart';
 export './widget/widgets/notification_item_widget.dart';
